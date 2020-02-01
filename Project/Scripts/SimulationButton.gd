3947f6ebef9b8eb7472c84ecb95a57ea678eabe8
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-01 18:25:40 +0200

    Timer for victory conditions

--- Project/Scripts/SimulationButton.gd
+++ Project/Scripts/SimulationButton.gd
@@ -2,19 +2,24 @@ extends Control
 
 var isStart = true
 var isDefaultSpeed = true
+var currentLevelNode
 
 func _on_TextureButton_pressed():
 	if(isStart):
 		get_tree().call_group("Blocks", "on_Block_enablePhysics")
+		currentLevelNode.get_node("Timer").start()
 		isStart = false
 	else:
 		var sceneLoader = get_node("/root/Root/SceneLoader")
 		sceneLoader.change_scene(sceneLoader.currentScene)
+		currentLevelNode.get_node("Timer").stop()
 		isStart = true
+		
 	pass # Replace with function body.
 
 
 func _on_SpeedButton_pressed():
+	var timer = currentLevelNode.get_node("Timer")
 	if(isDefaultSpeed):
 		Engine.time_scale = 2
 		isDefaultSpeed = false
