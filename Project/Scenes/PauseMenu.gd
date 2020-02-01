2622cf504f4b954711a788a104ceea406f9b844c
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-01 19:58:22 +0200

    Pause menu signals

--- Project/Scenes/PauseMenu.gd
+++ Project/Scenes/PauseMenu.gd
@@ -1,4 +1,23 @@
 extends Control
 
+signal resume_game
+signal open_menu
+signal quit_game
+
 func _ready():
+	pass
+
+
+func _on_ResumeButton_pressed():
+	emit_signal("resume_game")
+	pass
+
+
+func _on_MenuButton_pressed():
+	emit_signal("open_menu")
+	pass
+
+
+func _on_QuitButton_pressed():
+	emit_signal("quit_game")
 	pass
