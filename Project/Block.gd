0e0ae49a48baffd11258ffaf55d608af316f1ddb
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-01 14:55:27 +0200

    Bought parts to move cursor automatically

--- Project/Block.gd
+++ Project/Block.gd
@@ -3,6 +3,8 @@ extends Node2D
 export var isStatic = false
 export var isDraggable = false
 var can_drag = false
+var offset
+var firstBuy = false
 
 func _ready():
 	$RigidBody2D.mode = RigidBody2D.MODE_STATIC
@@ -12,6 +14,7 @@ func _ready():
 			mass += 1
 	$RigidBody2D.mass = mass
 	#print(get_class() + " Mass: " + str(mass))
+	offset = $RigidBody2D.position-position
 	pass
 
 func on_Block_enablePhysics():
@@ -25,13 +28,18 @@ func _on_RigidBody2D_input_event(viewport, event, shape_idx):
 			can_drag = event.pressed
 
 func _process(delta):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_drag:
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		if(firstBuy):
+			firstBuy = false
+	elif (Input.is_mouse_button_pressed(BUTTON_LEFT) and can_drag) or firstBuy:
 		var curPos = get_global_mouse_position()
-		var rigidPos = $RigidBody2D.position
-		var x = fmod(curPos.x, 8)
-		var y = fmod(curPos.y, 8)
-		
-		curPos.x -= x
-		curPos.y -= y
 			
-		position = curPos + (rigidPos-position)
+		print(str(offset) + " : " + str(position))
+		position = curPos-offset		
+		var x = fmod(position.x, 8)
+		var y = fmod(position.y, 8)
+		if x < 4:
+			position.x -= x
+		else:
+			position.x += (8-x)
+		position.y -= y
