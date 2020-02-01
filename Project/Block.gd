3964f93de5289be4755709a3d890cdcfa70c3cfb
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-01 09:31:56 +0200

    Changes to block scrip

--- Project/Block.gd
+++ Project/Block.gd
@@ -16,6 +16,7 @@ func on_Block_enablePhysics():
 	
 
 func _on_RigidBody2D_input_event(viewport, event, shape_idx):
+	print(isDraggable)
 	if $RigidBody2D.mode == RigidBody2D.MODE_STATIC and isDraggable:
 		if event is InputEventMouseButton:
 			can_drag = event.pressed
@@ -23,4 +24,17 @@ func _on_RigidBody2D_input_event(viewport, event, shape_idx):
 
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_drag:
-		position = get_global_mouse_position()
+		var curPos = get_global_mouse_position()
+		var x = fmod(curPos.x, 8)
+		var y = fmod(curPos.y, 8)
+		if x < 4:
+			curPos.x -= x
+		else:
+			curPos.x += (8-x)
+		
+		if y < 4:
+			curPos.y -= y
+		else:
+			curPos.y += (8-y)
+			
+		position = curPos
