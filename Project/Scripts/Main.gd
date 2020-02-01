14b1fcbbbfe1c329210e6144bc6cd52c10b974c9
kale <7550178+kalehead@users.noreply.github.com>
2020-02-01 15:23:49 +0200

    Stars start + globals

--- Project/Scripts/Main.gd
+++ Project/Scripts/Main.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 func _ready():
-	OS.window_size = Vector2(OS.window_size.x * 4, OS.window_size.y * 4)
+	OS.window_size = Vector2(OS.window_size.x * 3, OS.window_size.y * 3)
 	$CanvasLayer/SimulationButton.hide()
 	$CanvasLayer/PartShop.hide()
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.window_size
+	OS.window_position = screen_size*0.5 - window_size*0.5
 	pass
 
 
