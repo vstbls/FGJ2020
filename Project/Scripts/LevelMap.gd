1d869075855ad7cb85d1169baeb9a1a13b24246d
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-02 11:57:18 +0200

    Menu button in map

--- Project/Scripts/LevelMap.gd
+++ Project/Scripts/LevelMap.gd
@@ -8,3 +8,9 @@ func _ready():
 
 func _on_LevelButton_level_picked(levelname):
 	emit_signal("level_select", levelname)
+
+
+func _on_MenuButton_pressed():
+	hide()
+	get_node("/root/Root/CanvasLayer/Menu").show()
+	pass
