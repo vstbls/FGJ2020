b97fc8d123fbbedc4cc048ef4fdebcaa965a8b3f
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-02 13:43:23 +0200

    Timer

--- Project/Scripts/Level1.gd
+++ Project/Scripts/Level1.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 var firstStage = true
 var timeOut = 3
+var timeLeft = 5
+var timeLeftCurrent = 5
 var tomPos
 var stars
 
 func _ready():
 	stars = 0
 	tomPos = $NPC/RigidBody2D.position
+	timeLeftCurrent = timeLeft
+	$TimeLeft.text = "Time Left: " + str(timeLeftCurrent)
 
 func _on_Timer_timeout():
 	var curPos = $NPC/RigidBody2D.position
@@ -17,6 +21,7 @@ func _on_Timer_timeout():
 			stars = 1
 		$Timer.wait_time = 3
 		$Timer.start()
+		$Countdown.start()
 		firstStage = false
 	else:
 		blockCount = get_node("/root/Root/SceneLoader/CanvasLayer").get_child_count() - 1
@@ -24,9 +29,16 @@ func _on_Timer_timeout():
 			stars = 2
 			if blockCount == 1:
 				stars = 3
+		$Countdown.stop()
 		var levelComplete = load("res://Scenes/LevelComplete.tscn")
 		var complete_instance = levelComplete.instance()
 		complete_instance.set_text("Blocks used: " + str(blockCount))
 		complete_instance.set_victory(stars)
 		get_node("/root/Root/TransitionLayer").add_child(complete_instance)
 		if Globals.levelstats["Level1"].stars < stars: Globals.levelstats["Level1"].stars = stars
+
+
+func _on_Countdown_timeout():
+	timeLeftCurrent -= 1
+	$TimeLeft.text = "Time Left: " + str(timeLeftCurrent)
+	pass
--- Project/Scripts/SimulationButton.gd
+++ Project/Scripts/SimulationButton.gd
@@ -8,13 +8,17 @@ func _on_TextureButton_pressed():
 	if(isStart):
 		get_tree().call_group("Blocks", "on_Block_enablePhysics")
 		currentLevelNode.get_node("Timer").start()
+		currentLevelNode.get_node("Countdown").start()
 		isStart = false
 	else:
 		var sceneLoader = get_node("/root/Root/SceneLoader")
 		sceneLoader.change_scene(sceneLoader.currentScene)
 		var timer = currentLevelNode.get_node("Timer")
+		var countDown = currentLevelNode.get_node("CountDown")
 		timer.stop()
 		timer.wait_time = currentLevelNode.timeOut
+		countDown.stop()
+		countDown.wait_time = currentLevelNode.timeLeft
 		isStart = true
 		
 	pass # Replace with function body.
